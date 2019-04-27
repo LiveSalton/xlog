8ad09f4e20043619104337becfb6780949a0ea69
jinsheng.wu2 <jinsheng.wu2@zhenai.com>
2019-04-27 11:32:16 +0800

    update readme

--- sample/src/main/java/com/salton123/xlog/demo/XLogSampleApplication.java
+++ sample/src/main/java/com/salton123/xlog/demo/XLogSampleApplication.java
@@ -2,8 +2,11 @@ package com.salton123.xlog.demo;
 
 import android.app.Application;
 
+import com.salton123.log.Utils;
 import com.salton123.log.XLog;
 import com.salton123.log.XLogConfig;
+
+import java.io.File;
 
 /**
  * User: newSalton@outlook.com
@@ -17,7 +20,11 @@ public class XLogSampleApplication extends Application {
     public void onCreate() {
         super.onCreate();
         XLog.config(new XLogConfig()
-                .setDebugable(true)
-                .setWhetherToSaveLog(true));
+                .setDebugable(BuildConfig.DEBUG) //whether print info to logcat
+                .setWhetherToSaveLog(true)  //whether save log to file
+                .setLogDefaultSplitSize(1)  //split log size , unit m
+                .setSavePath(Utils.getDefaultPath() + getPackageName() + File.separator) //log save path
+                .setLogDeleteDelayDay(10)   //log delete delay day
+        );
     }
 }
