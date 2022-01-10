2fe5cd96a3359aae6621481c54ab3f9039a7a25f
wujinsheng1 <wujinsheng1@joyy.com>
2022-01-10 17:48:38 +0800

    DEMO 验证功能

--- sample/src/main/java/com/salton123/xlog/demo/MainActivity.kt
+++ sample/src/main/java/com/salton123/xlog/demo/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.salton123.xlog.demo
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.salton123.log.Utils
 import com.salton123.log.XLog
-import java.io.File
 import kotlinx.android.synthetic.main.aty_main.tvHello
 
 /**
@@ -25,35 +23,50 @@ class MainActivity : AppCompatActivity(), PermissionsUtil.IPermissionsCallback {
     override fun onPermissionsDenied(requestCode: Int, vararg permission: String?) {
     }
 
+    var index = 0L
     val permissionsUtil by lazy { PermissionsUtil.with(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.aty_main)
         tvHello.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                XLog.i(this, "hello")
-                val file = File(Utils.getDefaultPath() + packageName)
-                if (!file.exists()) {
-                    val ret = file.parentFile.mkdirs()
-                    XLog.i(this, "ret:$ret")
+//                XLog.i(this, "hello")
+//                val file = File(Utils.getDefaultPath() + packageName)
+//                if (!file.exists()) {
+//                    val ret = file.parentFile.mkdirs()
+//                    XLog.i(this, "ret:$ret")
+//                }
+                val sharedPreferences = getSharedPreferences("test", Context.MODE_PRIVATE)
+                sharedPreferences.edit().apply {
+                    this.putLong("index", index)
+                    commit()
+                    XLog.e("MainActivity", "hello ${index}")
                 }
+                System.exit(0)
             }
         })
-        Log.i("MainActivity", "hello")
-
         val storage = arrayOf(
             PermissionsUtil.Permission.Storage.READ_EXTERNAL_STORAGE,
             PermissionsUtil.Permission.Storage.WRITE_EXTERNAL_STORAGE
         )
         permissionsUtil.requestCode(1).permissions(*storage).request()
+        val sharedPreferences = getSharedPreferences("test", Context.MODE_PRIVATE)
+//        sharedPreferences.edit().apply {
+//            this.
+//        }
+        index = sharedPreferences.getLong("index", 0)
         Thread {
             while (true) {
-                XLog.i("MainActivity", "MainActivity hello")
-//                Thread.sleep(1000)
-                XLog.w(this, "MainActivityhello")
-                XLog.v(this, "MainActivityhello")
-                XLog.d(this, "MainActivityhello")
-                XLog.e(this, "MainActivityhello")
+                XLog.i("MainActivity", "hello ${index++}")
+                XLog.w("MainActivity", "hello ${index++}")
+                XLog.e("MainActivity", "hello ${index++}")
+                XLog.v("MainActivity", "hello ${index++}")
+                XLog.d("MainActivity", "hello ${index++}")
+//                Thread.sleep(100)
+//                XLog.w(this, "MainActivityhello")
+//                XLog.v(this, "MainActivityhello")
+//                XLog.d(this, "MainActivityhello")
+//                XLog.e(this, "MainActivityhello")
             }
         }.start()
     }
@@ -68,5 +81,10 @@ class MainActivity : AppCompatActivity(), PermissionsUtil.IPermissionsCallback {
         //监听跳转到权限设置界面后再回到应用
         permissionsUtil.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
     }
 }
